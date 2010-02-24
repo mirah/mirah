0ecff417503be91b386eb512e775d5d104247c2d
Charles Oliver Nutter <headius@headius.com>
2010-02-24 15:59:30 +0000

    Fix optional args test, which wasn't testing anything.

--- test/test_jvm_compiler.rb
+++ test/test_jvm_compiler.rb
@@ -1867,15 +1867,17 @@ class TestJVMCompiler < Test::Unit::TestCase
     end
 
     def test_optional_args
-    cls, = compile(<<-EOF)
-      def foo(a, b = 1, c = 2)
-        puts a; puts b; puts c
+      cls, = compile(<<-EOF)
+        def foo(a, b = 1, c = 2)
+          puts a; puts b; puts c
+        end
+        foo(0)
+        foo(0,0)
+        foo(0,0,0)
+      EOF
+      assert_output("0\n1\n2\n0\n0\n2\n0\n0\n0") do
+        cls.main([].to_java :string)
       end
-      foo(0)
-      foo(0,0)
-      foo(0,0,0)
-    EOF
-    assert_output("0\n1\n2\n0\n0\n2\n0\n0\n0")
     end
   end
 end
