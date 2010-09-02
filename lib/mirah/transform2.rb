29d2742a938a7fb0554d6420ac158f6f486882bd
Ryan Brown <ribrdb@google.com>
2010-09-02 08:59:31 -0700

    Start using new parser

---
+++ lib/mirah/transform2.rb
@@ -0,0 +1,19 @@
+module Duby::AST
+  class TransformHelper
+    def initialize(transformer)
+      @mirah = transformer
+    end
+
+    def position(node)
+      @mirah.position(node)
+    end
+
+    def transform_script(node, parent)
+      Script.new(parent, position(node)) {|script| [@mirah.transform(node.children[0], script)]}
+    end
+
+    def transform_fixnum(node, parent)
+      AST::fixnum(parent, position(node), node[1].intValue)
+    end
+  end
+end
