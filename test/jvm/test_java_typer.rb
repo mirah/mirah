d4070feefa9f35f760991be13790ae3585612b59
Nick Howard <ndh@baroquebobcat.com>
2011-07-03 13:47:22 -0600

    moved jvm tests into subdirectory

--- test/jvm/test_java_typer.rb
+++ test/jvm/test_java_typer.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+$:.unshift File.join(File.dirname(__FILE__),'../..','lib')
 
 require 'test/unit'
 require 'mirah'
