f0499f519fd8738fd5be53bd172435549c5b88e0
Ryan Brown <ribrdb@google.com>
2012-04-19 12:19:57 -0700

    Don't auto-generate a class or a main method unless needed

--- lib/mirah/jvm/compiler/base.rb
+++ lib/mirah/jvm/compiler/base.rb
@@ -19,6 +19,7 @@ module Mirah
       java_import 'mirah.lang.ast.ClassDefinition'
       java_import 'mirah.lang.ast.StaticMethodDefinition'
       java_import 'mirah.lang.ast.SimpleNodeVisitor'
+      java_import 'mirah.lang.ast.NodeScanner'
       
       class Base < SimpleNodeVisitor
         attr_accessor :filename, :method, :static, :class
@@ -99,6 +100,43 @@ module Mirah
           log "...done!"
         end
 
+        # Scans the top level of a file to see if it contains anything outside of a ClassDefinition.
+        class ScriptScanner < NodeScanner
+          attr_reader :found_other, :found_method
+          def enterDefault(node, arg)
+            @found_other = true
+            false
+          end
+          def enterMethodDefinition(node, arg)
+            @found_method = true
+            false
+          end
+          def enterStaticMethodDefinition(node, arg)
+            @found_method = true
+            false
+          end
+          def enterConstructorDefinition(node, arg)
+            @found_method = true
+            false
+          end
+          def enterPackage(node, arg)
+            # ignore
+            false
+          end
+          def enterClassDefinition(node, arg)
+            # ignore
+            false
+          end
+          def enterImport(node, arg)
+            # ignore
+            false
+          end
+          def enterNodeList(node, arg)
+            # Scan the children
+            true
+          end
+        end
+
         def visitScript(script, expression)
           @static = true
           @filename = File.basename(script.position.source.name)
@@ -106,28 +144,33 @@ module Mirah
           @type = @typer.type_system.type(get_scope(script), classname)
           @file = file_builder(@filename)
           body = script.body
-          body = body.get(0) if body.size == 1
-          if body.class != ClassDefinition
-
+          scanner = ScriptScanner.new
+          scanner.scan(body, expression)
+          need_class = scanner.found_method || scanner.found_other
+          if need_class
             @class = @type.define(@file)
-            with :method => @class.main do
-              log "Starting main method"
+            if scanner.found_other
+              # Generate the main method
+              with :method => @class.main do
+                log "Starting main method"
 
-              @method.start
-              @current_scope = get_scope(script)
-              declare_locals(@current_scope)
-              begin_main
+                @method.start
+                @current_scope = get_scope(script)
+                declare_locals(@current_scope)
+                begin_main
 
-              prepare_binding(script) do
-                visit(body, false)
+                prepare_binding(script) do
+                  visit(body, false)
+                end
+
+                finish_main
+                @method.stop
               end
-
-              finish_main
-              @method.stop
+              log "Main method complete!"
+            else
+              visit(body, false)
             end
             @class.stop
-
-            log "Main method complete!"
           else
             visit(body, false)
           end
