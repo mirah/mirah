e00ab4bc6a3df6853eb3b951955660f378ef3a35
Nick Howard <ndh@baroquebobcat.com>
2012-04-22 23:35:13 -0600

    comment out bootclasspath test--there is no bootclasspath yet
    to get it to work, we need to port these commits over (in asc order)
    
      d11f694bcc7654b69aee8cbc1d1af9ff56019a77 actually use the bootclasspath in the bootstrap_loader
      6f293d8d9b69693bb062d47794a01f12060168be fix and test(barely) IsolatedResourceLoader
      cff273c5130763bf21c81167e72c91d0cab5ca48 add tests for MirahClassLoader
      98adac4d87129eed4106e7daef2d492b4bcabf95 add some comments on class loaders
      886bc5f7e9536f75a60c0f9e90c6bdb17e043579 test that bootclasspath arg sets the bootclasspath
      2642a28ebbcc1f9b2502d3b5b4ea13a76ffa56a4 add tab to cli help for bootclasspath
      97cee78023c0582bba4669a63f5db5a744087f14 Support specifying --bootclasspath

--- test/core/util/argument_processor_test.rb
+++ test/core/util/argument_processor_test.rb
@@ -27,16 +27,16 @@ class ArgumentProcessorTest < Test::Unit::TestCase
     assert_equal 1, processor.exit_status_code
   end
 
-  def test_arg_bootclasspath_sets_bootclasspath_on_type_factory_ugh
-    Mirah::AST.type_factory = Mirah::JVM::Types::TypeFactory.new # global state grumble grumble
-
-    path = "class:path"
-    state = Mirah::Util::CompilationState.new
-    processor = Mirah::Util::ArgumentProcessor.new state, ["--bootclasspath", path]
-    processor.process
-
-    assert_equal path, Mirah::AST.type_factory.bootclasspath
-  end
+#  def test_arg_bootclasspath_sets_bootclasspath_on_type_factory_ugh
+#    Mirah::AST.type_factory = Mirah::JVM::Types::TypeFactory.new # global state grumble grumble
+#
+#    path = "class:path"
+#    state = Mirah::Util::CompilationState.new
+#    processor = Mirah::Util::ArgumentProcessor.new state, ["--bootclasspath", path]
+#    processor.process
+#
+#    assert_equal path, Mirah::AST.type_factory.bootclasspath
+#  end
 
   def test_dash_j_fails_when_not_compiling
     state = Mirah::Util::CompilationState.new
