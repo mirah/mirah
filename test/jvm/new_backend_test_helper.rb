e1212f67d2a96ebfebf8f7752e411e49f72526ce
Nick Howard <ndh@baroquebobcat.com>
2013-04-07 15:02:29 -0600

    bump copyright on a couple test files

--- test/jvm/new_backend_test_helper.rb
+++ test/jvm/new_backend_test_helper.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Mirah project authors. All Rights Reserved.
+# Copyright (c) 2010-2013 The Mirah project authors. All Rights Reserved.
 # All contributing project authors may be found in the NOTICE file.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
