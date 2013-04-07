8359686a81c2019b3f59b762fdc8b9483d2f5a90
Nick Howard <ndh@baroquebobcat.com>
2013-04-07 15:03:06 -0600

    move common test helper dependencies to test_helper

--- test/jvm/bytecode_test_helper.rb
+++ test/jvm/bytecode_test_helper.rb
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 require 'test_helper'
-require 'stringio'
-require 'fileutils'
 require 'set'
 
 module JVMCompiler
--- test/jvm/new_backend_test_helper.rb
+++ test/jvm/new_backend_test_helper.rb
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 require 'bytecode_test_helper'
-require 'stringio'
-require 'fileutils'
 
 module JVMCompiler
   def compiler_type
