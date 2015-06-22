8f2443dc8bce30195f1543df974c79ac5f8e1466
Felix von Ferey <felix+mirah@noreply.ferey.de>
2015-06-22 12:31:41 +0200

    Test []#join().

--- test/jvm/extensions/collection_extensions_test.rb
+++ test/jvm/extensions/collection_extensions_test.rb
@@ -23,5 +23,37 @@ class CollectionExtensionsTest < Test::Unit::TestCase
     assert_run_output("a1c\n", cls)
   end
 
+  def test_java_array_join_empty
+    cls, = compile(<<-EOF)
+      x = int[0].join
+      puts x
+    EOF
+    assert_run_output("\n", cls)
+  end
+
+  def test_java_array_join_single
+    cls, = compile(<<-EOF)
+      x = ["a"].to_a(String).join
+      puts x
+    EOF
+    assert_run_output("a\n", cls)
+  end
+
+  def test_java_array_join_multiple
+    cls, = compile(<<-EOF)
+      x = ["a",1,"c"].to_a(Object).join
+      puts x
+    EOF
+    assert_run_output("a1c\n", cls)
+  end
+
+  def test_primitve_java_array_join_multiple
+    cls, = compile(<<-EOF)
+      x = int[4].join
+      puts x
+    EOF
+    assert_run_output("0000\n", cls)
+  end
+
 end
 
