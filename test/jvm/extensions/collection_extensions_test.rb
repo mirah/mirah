72648944dd5fdaeb5cc33a5e4f8f14bd05628cb7
Felix von Ferey <felix+mirah@noreply.ferey.de>
2015-06-22 12:38:14 +0200

    Test #join(separator).

--- test/jvm/extensions/collection_extensions_test.rb
+++ test/jvm/extensions/collection_extensions_test.rb
@@ -55,5 +55,61 @@ class CollectionExtensionsTest < Test::Unit::TestCase
     assert_run_output("0000\n", cls)
   end
 
+  def test_mirah_array_join_separator_empty
+    cls, = compile(<<-EOF)
+      x = [].join(",")
+      puts x
+    EOF
+    assert_run_output("\n", cls)
+  end
+
+  def test_mirah_array_join_separator_single
+    cls, = compile(<<-EOF)
+      x = ["a"].join(",")
+      puts x
+    EOF
+    assert_run_output("a\n", cls)
+  end
+
+  def test_mirah_array_join_separator_multiple
+    cls, = compile(<<-EOF)
+      x = ["a",1,"c"].join(",")
+      puts x
+    EOF
+    assert_run_output("a,1,c\n", cls)
+  end
+
+  def test_java_array_join_separator_empty
+    cls, = compile(<<-EOF)
+      x = int[0].join(",")
+      puts x
+    EOF
+    assert_run_output("\n", cls)
+  end
+
+  def test_java_array_join_separator_single
+    cls, = compile(<<-EOF)
+      x = ["a"].to_a(String).join(",")
+      puts x
+    EOF
+    assert_run_output("a\n", cls)
+  end
+
+  def test_java_array_join_separator_multiple
+    cls, = compile(<<-EOF)
+      x = ["a",1,"c"].to_a(Object).join(",")
+      puts x
+    EOF
+    assert_run_output("a,1,c\n", cls)
+  end
+
+  def test_primitve_java_array_join_separator_multiple
+    cls, = compile(<<-EOF)
+      x = int[4].join(",")
+      puts x
+    EOF
+    assert_run_output("0,0,0,0\n", cls)
+  end
+
 end
 
